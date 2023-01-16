be21d038ba2c1f51b803bfb19a1c11fa0432259c
VasviGarg <gargvasvi2004@gmail.com>
2023-01-16 18:42:20 +0000

    index.js edited

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function(req,res) {
+  const ipaddress = req.headers.host
+  const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
+  res.json({
+    ipaddress, language, software
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
